30e156e3b49efdf1281c4d0e86520a12a46809e2
victorlin27 <97265213+victorlin27@users.noreply.github.com>
2023-05-08 19:48:32 -0700

    Add files via upload

---
+++ loop challenges/loopchallenges.js
@@ -0,0 +1,16 @@
+for(var i=1; i<21; i++){
+    if(i % 2 ==0)
+    console.log()
+    else
+    console.log(i)
+}
+
+for(var i=100; i>-1; i--){
+    if(i % 3 == 0)
+    console.log(i)
+}
+
+for(var i=4; i>-4; i-=1.5){
+    console.log(i)
+}
+
